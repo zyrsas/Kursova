efbb23788bfe3878dcf7f3e4834199fa54967deb
Sasha <zyrsas@gmail.com>
2015-04-11 09:32:48 +0300

    added testUser form set with file

---
+++ Tester/SettingTest.cpp
@@ -0,0 +1,6 @@
+#include "SettingTest.h"
+
+
+SettingTest::SettingTest(void)
+{
+}
---
+++ Tester/SettingTest.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+using namespace std;
+
+class SettingTest
+{
+public:
+	SettingTest(void);
+	static string author;
+	static int time;
+	static int ball;
+};
+
